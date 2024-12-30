bf70c2b5be8fdda8a72a9e5cb00ab09b7b373847
GanievAhmadsher <ganievahmadsher6205@gmail.com>
2024-12-30 19:27:39 +0500

    Node.js 4 - Homework pushed

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 import path from "node:path";
 import fs from "node:fs";
-const activePath = path.join(process.cwd(), "fs.txt");
-const activeWrite = path.join(process.cwd(), "fs2.txt");
-fs.readFile(activePath, (err, data) => {
-  fs.writeFile(activeWrite, data, (err) => {
-    console.log(err);
-  });
-});
+const activePath = path.join(process.cwd(), "dars2.wmv");
+const activeWrite = path.join(process.cwd(), "fs2.mp4");
+// fs.readFile(activePath, (err, data) => {
+//   fs.writeFile(activeWrite, data, (err, data) => {
+//     console.log(err);
+//   });
+// });
+
+// fs.readFile(activePath, (err, data) => {
+//   fs.writeFile(activeWrite, data, (err) => {
+//     console.log(err);
+//   });
+// });
 
 // let buf = Buffer.from("ganievahmadsher6205@gmail.com");
 // console.log(buf);
 // console.log(buf.toString("utf-8"));
+// async function name(params) {
+//     fs.readFile(activePath,'utf-8')
+// }
+
+// fs.statSync(); // ? file info
+// fs.existsSync(); // ? file (has or don't have)
+
+// fs.readFile()
---
+++ home.js
@@ -0,0 +1,86 @@
+// * ============================== Homework4: (work with fs) ==============================
+import { error } from "node:console";
+import fs from "node:fs";
+import path from "node:path";
+// ? =============== Masala1 =============== start
+// fs.writeFile(
+//   path.join(process.cwd(), "for_home", "test.txt"),
+//   "Salom NodeJS",
+//   (err, data) => {}
+// );
+// ? =============== Masala1 =============== end
+
+// ? =============== Masala2 =============== start
+// fs.readFile(
+//   path.join(process.cwd(), "for_home", "test.txt"),
+//   "utf8",
+//   (err, data) => {
+//     console.log(data);
+//   }
+// );
+// ? =============== Masala2 =============== end
+
+// ? =============== Masala3 =============== start
+// fs.mkdir(
+//   path.join(process.cwd(), "for_home", "yangi_papka"),
+//   (err, data) => {}
+// );
+// ? =============== Masala3 =============== end
+
+// ? =============== Masala4 =============== start
+// fs.copyFile(
+//   path.join(process.cwd(), "for_home", "test.txt"),
+//   path.join(process.cwd(), "for_home", "test_copy.txt"),
+//   (err, data) => {}
+// );
+// ? =============== Masala4 =============== end
+
+// ? =============== Masala5 =============== start
+// fs.unlink(
+//   path.join(process.cwd(), "for_home", "test_copy.txt"),
+//   (err, data) => {}
+// );
+// ? =============== Masala5 =============== end
+
+// ? =============== Masala6 =============== start
+// fs.appendFile(
+//   path.join(process.cwd(), "for_home", "test.txt"),
+//   "\nwork with fs",
+//   (err, data) => {}
+// );
+// ? =============== Masala6 =============== end
+
+// ? =============== Masala7 =============== start
+// fs.readdir(
+//   path.join(process.cwd(), "for_home", "yangi_papka"),
+//   {},
+//   (err, data) => {
+//     console.log(data);
+//   }
+// );
+// ? =============== Masala7 =============== end
+
+// ? =============== Masala8 =============== start
+// let isHas = fs.existsSync(path.join(process.cwd(), "for_home", "test.txt"));
+// if (isHas) {
+//   console.log("File mavjud");
+// } else {
+//   console.log("File yo'q");
+// }
+// ? =============== Masala8 =============== end
+
+// ? =============== Masala9 =============== start
+// fs.stat(path.join(process.cwd(), "for_home", "test.txt"), (err, data) => {
+//   console.log(`Hajmi: ${data.size}, Yaratilgan vaqti: ${data.birthtime}`);
+// });
+// ? =============== Masala9 =============== end
+
+// ? =============== Masala10 =============== start
+// fs.rename(
+//   path.join(process.cwd(), "for_home", "test.txt"),
+//   path.join(process.cwd(), "for_home", "yangi_papka", "new_test.txt"),
+//   (err, data) => {}
+// );
+// ? =============== Masala10 =============== end
+
+// * ============================== Homework4: (work with fs) ==============================
